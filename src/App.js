77252e21751bb24f85a73110179ba945f1ff3165
borwi4ok <donnos98@gmail.com>
2020-10-31 04:04:49 +0300

    added primary navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import './App.css'
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Main from './pages/main/Main'
+import Inventory from './pages/inventory/Inventory'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <React.Fragment>
+      <Switch>
+        <Route path='/' component={Main}></Route>
+        <Route path='/inventory' component={Inventory}></Route>
+      </Switch>
+    </React.Fragment>
+  )
 }
 
 export default App
