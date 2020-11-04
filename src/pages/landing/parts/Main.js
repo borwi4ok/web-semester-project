83f92eadc6e0f206afbf77c124c8d30d20011e71
borwi4ok <donnos98@gmail.com>
2020-11-05 01:18:23 +0300

    add some adaptable properties + add fonts

--- src/pages/landing/parts/Main.js
+++ src/pages/landing/parts/Main.js
@@ -5,8 +5,15 @@ export default function Main() {
   return (
     <div className='container'>
       <div className='wrapperImages'>
-        <div className='leftImage image'></div>
-        <div className='rightImage image'></div>
+        <div className='wrapperImages__left'></div>
+        <div className='wrapperImages__right'></div>
+      </div>
+      <div className='tableOfUsers'>
+        <div className='tableOfUsers__headlines'>
+          <span>Nickname</span>
+          <span>COST</span>
+        </div>
+        <div></div>
       </div>
     </div>
   )
