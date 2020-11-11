5b5318b94b2cb378bcfb07f78371adea5ea2cea2
ComradAlex <sigarevaleksandr95@gmail.com>
2020-11-12 00:38:46 +0300

    added navbar

--- src/pages/landing/parts/Main.js
+++ src/pages/landing/parts/Main.js
@@ -1,40 +1,55 @@
+
 import React from 'react'
 import styles from './Main.module.css'
 
 export default function Main() {
   return (
-    <div className={styles.containerMain}>
-      <div className={styles.wrapperImages}>
-        <div className={styles.wrapperImages__left}></div>
-        <div className={styles.wrapperImages__right}></div>
-      </div>
-      <div className={styles.tableOfUsers}>
-        <div className={styles.tableOfUsers__headlines}>
-          <span>Nickname</span>
-          <span>COST</span>
+      <div className={styles.containerMain}>
+        <div className="navbar" >
+          <div className="logo">
+            <img src='../media/logo.png'/>
+          </div>
+          <div className="links">
+            <a>Main</a>
+            <a>About us</a>
+            <a>Have problems?</a>
+            <button className="button">
+              <a>Sign in</a>
+            </button>
+          </div>
+
         </div>
-        {/* Добавить li-component */}
-        <ul className={styles.listOfUsers}>
-          <div>
-            <li>asd</li>
+        <div className={styles.wrapperImages}>
+          <div className={styles.wrapperImages__left}></div>
+          <div className={styles.wrapperImages__right}></div>
+        </div>
+        <div className={styles.tableOfUsers}>
+          <div className={styles.tableOfUsers__headlines}>
+            <span>Nickname</span>
+            <span>COST</span>
           </div>
-          <div>
-            <li>asd</li>
-          </div>
-          <div>
-            <li>asd</li>
-          </div>
-          <div>
-            <li>asd</li>
-          </div>
-          <div>
-            <li>asd</li>
-          </div>
-          <div>
-            <li>asd</li>
-          </div>
-        </ul>
+          {/* Добавить li-component */}
+          <ul className={styles.listOfUsers}>
+            <div>
+              <li>asd</li>
+            </div>
+            <div>
+              <li>asd</li>
+            </div>
+            <div>
+              <li>asd</li>
+            </div>
+            <div>
+              <li>asd</li>
+            </div>
+            <div>
+              <li>asd</li>
+            </div>
+            <div>
+              <li>asd</li>
+            </div>
+          </ul>
+        </div>
       </div>
-    </div>
   )
 }
