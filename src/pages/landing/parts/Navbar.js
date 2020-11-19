9c2f15de5fa2d4a9d94ac9919fc1ff43054a81df
ComradAlex <sigarevaleksandr95@gmail.com>
2020-11-19 17:43:48 +0300

    added adaptive navbar

--- src/pages/landing/parts/Navbar.js
+++ src/pages/landing/parts/Navbar.js
@@ -7,12 +7,14 @@ export default function Navbar() {
     <div className={styles.container_navbar}>
       <div className={styles.container__logo}></div>
       <div className={styles.container__links}>
-            <a>Main</a>
-            <a>About us</a>
-            <a>Have problems?</a>
-            <button className={styles.container__button}>
-              <a>Sign in</a>
-            </button>
+
+              <a>Main</a>
+              <a>About us</a>
+              <a>Have problems?</a>
+              <button className={styles.container__button}>
+                  <a>Sign in</a>
+              </button>
+
       </div>
     </div>
   )
