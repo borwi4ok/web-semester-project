50a5df3679cf198ed6104ed45ee648e3aa94b306
ComradAlex <sigarevaleksandr95@gmail.com>
2020-11-19 08:15:18 +0300

    added /.idea to gitignore added user component

--- src/pages/inventory/parts/header/User.js
+++ src/pages/inventory/parts/header/User.js
@@ -3,21 +3,21 @@ import styles from './User.module.css'
 import SelectCurrency from './SelectCurrency'
 
 export default function User() {
-  return (
-    <div className={styles.container}>
-      <div className={styles.logoWrapper}>
-        <div className={styles.logo}></div>
-      </div>
-      <div className={styles.userInfo}>
-        <p>Nickname</p>
-        <div className={styles.totalCost}>
+    return (
+        <div className={styles.container}>
+            <div className={styles.logoWrapper}>
+                <div className={styles.logo}></div>
+            </div>
+            <div className={styles.userInfo}>
+                <p>Nickname</p>
+                <div className={styles.totalCost}>
           <span>
             Total cost:
             <span>&nbsp;&nbsp;1000$</span>
           </span>
+                </div>
+            </div>
+            <SelectCurrency />
         </div>
-      </div>
-      <SelectCurrency />
-    </div>
-  )
+    )
 }
